de9a5804a15bc2c32b0d1930930c95c8731d30f0
liamb-immalle <liam.brangers@immalle.be>
2018-01-28 16:06:31 +0100

    oplossing taak van Gitter

---
+++ groet.php
@@ -0,0 +1,8 @@
+
+<?php
+if(!isset($_COOKIE["name"])) {
+     echo "Hallo!";
+} else {
+     echo "Hallo, " . $_COOKIE["name"];
+}
+?>
---
+++ naamontvangen.php
@@ -0,0 +1,8 @@
+
+
+<form action="groet.php"><input type="submit" value="Klik hier voor een groet!"/>
+
+<?php
+echo "Bedankt!";
+setcookie("name",$_GET["name"],time() +(86400*30),"/");
+?>
